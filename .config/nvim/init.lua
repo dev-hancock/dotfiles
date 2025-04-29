d6dd0088a30c365928f68ed9fd8c7d06438e2de3
Adam Hancock <175297776+dev-hancock@users.noreply.github.com>
2025-04-29 21:10:43 +0100

    initial commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
