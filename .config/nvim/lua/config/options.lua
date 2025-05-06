9220ef0a454b384dd9964f2c46b2132acbe60b11
Adam Hancock <175297776+dev-hancock@users.noreply.github.com>
2025-05-06 13:31:01 +0100

    updated nvim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,11 +3,11 @@
 local opt = vim.opt
 
 -- Line numbers
-opt.number = true		-- Show absolute line numbers
-opt.relativenumber = true	-- Show relative Line numbers
+opt.number = true		      -- Show absolute line numbers
+opt.relativenumber = true -- Show relative Line numbers
 
 -- Tabs & Indentation
-opt.tabstop = 2			-- Number of spaces a <Tab> counts for
-opt.shiftwidth = 2		-- Number of spaces for auto-indent
-opt.expandtab = true		-- Use spaces instead of tabs
+opt.tabstop = 4			      -- Number of spaces a <Tab> counts for
+opt.shiftwidth = 4        -- Number of spaces for auto-indent
+opt.expandtab = true		  -- Use spaces instead of tabs
 opt.smartindent = true		-- Smart indentation
